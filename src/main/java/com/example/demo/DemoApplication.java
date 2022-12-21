3946d60a32982ea2da57db2a980f57a900c90d8a
Dragos Nutu <nutudragos@gmail.com>
2022-12-21 13:08:16 +0200

    Initial commit.

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
+@EnableScheduling
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+    @GetMapping("/hello")
+    String hello(@RequestParam(required = false, defaultValue = "World") String name) {
+        return "Hello %s".formatted(name);
+    }
+
+}
